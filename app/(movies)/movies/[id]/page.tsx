bc99a0b373b249da07d22f9435ad0f0578b1987a
moonhyejee <erica3211@naver.com>
2025-11-27 00:06:57 +0900

    3.0 Introduction

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,6 +1,8 @@
 export default async function MovieDetail({params}: {params: Promise<{ id: string }>
 }) {
     const {id} = await params;
+    // 영화 API 
+    // https://nomad-movies.nomadcoders.workers.dev/
     return (
         <div>
             <h1>Movie {id}</h1>
