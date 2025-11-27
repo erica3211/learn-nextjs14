c0dd52139af3c47a4c9632673720f3024e2d6c3e
moonhyejee <erica3211@naver.com>
2025-11-28 01:14:43 +0900

    #3.8 Conclusion 원복

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -2,9 +2,9 @@ import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
     await new Promise((resolve) => setTimeout(resolve, 2000));
-    throw new Error("오류 발생!");
-    // const response = await fetch(`${API_URL}/${id}/videos`);
-    // return response.json();
+    // throw new Error("오류 발생!");
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
