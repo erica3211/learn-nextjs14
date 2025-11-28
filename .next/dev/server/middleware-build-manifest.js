8bae594c570460a3535b921eeb994641ce476e31
moonhyejee <erica3211@naver.com>
2025-11-28 17:52:21 +0900

    #4.2 Movie Styles 강의 내용

--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -1,6 +1,31 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/_app": []
+    "/_app": [
+      "static/chunks/5b592_next_dist_compiled_919e2e8b._.js",
+      "static/chunks/5b592_next_dist_shared_lib_bf96f2fd._.js",
+      "static/chunks/5b592_next_dist_client_c2a67f2b._.js",
+      "static/chunks/5b592_next_dist_505e5b3b._.js",
+      "static/chunks/5b592_next_app_f1b25882.js",
+      "static/chunks/[next]_entry_page-loader_ts_07099891._.js",
+      "static/chunks/5b592_react-dom_118af9ce._.js",
+      "static/chunks/5b592_656d9f0f._.js",
+      "static/chunks/[root-of-the-server]__06e368bd._.js",
+      "static/chunks/Documents_learn-nextjs14_pages__app_2da965e7._.js",
+      "static/chunks/turbopack-Documents_learn-nextjs14_pages__app_ec909a29._.js"
+    ],
+    "/_error": [
+      "static/chunks/5b592_next_dist_compiled_919e2e8b._.js",
+      "static/chunks/5b592_next_dist_shared_lib_431c15ea._.js",
+      "static/chunks/5b592_next_dist_client_c2a67f2b._.js",
+      "static/chunks/5b592_next_dist_1b88696c._.js",
+      "static/chunks/5b592_next_error_e9971d43.js",
+      "static/chunks/[next]_entry_page-loader_ts_e831ed74._.js",
+      "static/chunks/5b592_react-dom_118af9ce._.js",
+      "static/chunks/5b592_656d9f0f._.js",
+      "static/chunks/[root-of-the-server]__10efbc0e._.js",
+      "static/chunks/Documents_learn-nextjs14_pages__error_2da965e7._.js",
+      "static/chunks/turbopack-Documents_learn-nextjs14_pages__error_3c421b03._.js"
+    ]
   },
   "devFiles": [],
   "polyfillFiles": [
